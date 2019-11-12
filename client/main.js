e9d890a35abe318067cabfde2348144b11ac2276
Pittman, Jamie <jamie.pittman@fmr.com>
2019-11-12 14:52:40 -0500

    initial commit

---
+++ client/main.js
@@ -0,0 +1,11 @@
+import './main.html';
+
+import React, { Component } from 'react';
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
+
+import App from '../imports/client/App';
+
+Meteor.startup(() => {
+  render(<App />, document.getElementById('render-target'));
+});
